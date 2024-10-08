e6b76d2982cdd14b3edc1caf78b66e6734ef7791
Ranjeet Kumar <143226519+RanjeetKumar63@users.noreply.github.com>
2024-10-08 15:11:12 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="text-center">Learn about redux</h1>
+      <AddTodo className="add" />
+      <Todos className="delete" />
     </div>
   );
-}
+};
 
 export default App;
